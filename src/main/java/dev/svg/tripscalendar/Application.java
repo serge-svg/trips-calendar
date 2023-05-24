b952d33499cdb26568825755ab39c50a9a858b7a
Sergi Vaqué Galdón <sergivaque@gmail.com>
2023-05-24 17:26:40 +0200

    Added @Value annotations

--- src/main/java/dev/svg/tripscalendar/Application.java
+++ src/main/java/dev/svg/tripscalendar/Application.java
@@ -1,14 +1,11 @@
 package dev.svg.tripscalendar;
 
-import dev.svg.tripscalendar.config.TripsCalendarProperties;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(TripsCalendarProperties.class)
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Trips calendar API", version = "1.0", description = "Trips calendar API Information"))
 public class Application {
--- src/main/java/dev/svg/tripscalendar/config/TripsCalendarProperties.java
+++
@@ -1,8 +0,0 @@
-package dev.svg.tripscalendar.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(value = "tc")
-public record TripsCalendarProperties (String welcomeMessage, String aboutMessage){
-
-}
