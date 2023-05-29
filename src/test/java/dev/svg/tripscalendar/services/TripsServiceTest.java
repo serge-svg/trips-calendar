b9b91b37d10f4f027de5075004bf2b8ec3236d54
Sergi Vaqué Galdón <sergivaque@gmail.com>
2023-05-29 18:18:54 +0200

    Added UT for TripService

--- src/test/java/dev/svg/tripscalendar/services/TripsServiceTest.java
+++ src/test/java/dev/svg/tripscalendar/services/TripsServiceTest.java
@@ -5,6 +5,7 @@ import dev.svg.tripscalendar.model.Trip;
 import dev.svg.tripscalendar.model.TripType;
 import dev.svg.tripscalendar.repository.TripsSDJdbcRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,8 +14,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
 
-import static org.mockito.Mockito.doReturn;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
+@DisplayName("Trips Service")
 @ExtendWith(MockitoExtension.class)
 class TripsServiceTest {
 
@@ -28,8 +31,7 @@ class TripsServiceTest {
 
     @BeforeEach
     void setUp() {
-        tripsService =  new TripsService(repository);
-        trip = new Trip(null,
+        trip = new Trip(1,
                 "Trip to somewhere",
                 Status.IDEA,
                 TripType.BEACH,
@@ -41,31 +43,50 @@ class TripsServiceTest {
 
     @Test
     void getAll() {
-        doReturn(Collections.singletonList(trip)).when(repository).findAll();
+        when(repository.findAll()).thenReturn(Collections.singletonList(trip));
 
+        assertThat(tripsService.getAll()).isNotEmpty();
     }
 
     @Test
     void getById() {
+        when(repository.findById(1)).thenReturn(java.util.Optional.of(trip));
+
+        assertThat(tripsService.getById(1)).isEqualTo(java.util.Optional.of(trip));
     }
 
     @Test
     void create() {
+        when(repository.save(any(Trip.class))).thenReturn(trip);
+        tripsService.create(trip);
+        verify(repository, times(1)).save(any(Trip.class));
     }
 
     @Test
     void update() {
+        when(repository.save(any(Trip.class))).thenReturn(trip);
+        tripsService.update(trip);
+        verify(repository, atLeastOnce()).save(any(Trip.class));
     }
 
     @Test
     void delete() {
+        doNothing().when(repository).deleteById(1);
+        tripsService.delete(1);
+        verify(repository, times(1)).deleteById(1);
     }
 
     @Test
     void findByStatus() {
+        when(repository.findByStatus("IDEA")).thenReturn(Collections.singletonList(trip));
+
+        assertThat(tripsService.findByStatus("IDEA")).isNotEmpty();
     }
 
     @Test
     void listByTripType() {
+        when(repository.listByTripType("BEACH")).thenReturn(Collections.singletonList(trip));
+
+        assertThat(tripsService.listByTripType("BEACH")).isNotEmpty();
     }
 }
